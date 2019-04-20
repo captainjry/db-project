ba6afd8ac2359028b8e79a89369902568fbfd37f
Jirayus Vorawan <captains9977@Jirayuss-MacBook-Pro.local>
2019-04-20 21:14:33 +0700

    DB

--- connect.php
+++ connect.php
@@ -1,5 +1,6 @@
 <?php
-    $mysqli = new mysqli('localhost','root','','bookstore');
+    $socket="/cloudsql/bookstore-236508:asia-southeast1:ta2tkwra";
+    $mysqli = new mysqli('null','root','','bookstore',$socket);
     if($mysqli->connect_errno) {
         echo $mysqli->connect_errno.": ".$mysqli->connect_error;
     }
